5ead52a25aa0e454c9779da41eb944d118b53247
Yasir Moosa <yasiryousafmoosa@gmail.com>
2026-09-14 21:56:26 +0100

    changes saved when closing vscode might need to revert back to old version

--- infra/modules/vpc/vpc_main.tf
+++ infra/modules/vpc/vpc_main.tf
@@ -72,7 +72,7 @@ resource "aws_route_table" "public_rt" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
-  tags = { Project = "ecs-practice" }
+
 }
 
 resource "aws_route_table" "private_rt" {
